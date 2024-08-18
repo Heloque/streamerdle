86114caaa6366bdfe731200735e7fd50b93c8ce1
Liranon <liranongaming@gmail.com>
2024-08-18 17:00:41 +0200

    fonction de hachage

--- src/utils/randomColor.js
+++ src/utils/randomColor.js
@@ -1,29 +1,47 @@
 import data from '../data.json';
 
-const getRandomElement = (array) => {
-  const currentIndex = localStorage.getItem('dailyElement')
-  let randomIndex;
-  do {
-    randomIndex = Math.floor(Math.random() * array.length);
-  } while (randomIndex === currentIndex)
-  return randomIndex;
+function hashDate(date, plafond) {
+  let [annee, mois, jour] = date.split('-').map(Number);
+
+  let valeurBase = annee * 10000 + mois * 100 + jour;
+
+  function mix(x) {
+      x = ((x >>> 16) ^ x) * 0x45d9f3b;
+      x = ((x >>> 16) ^ x) * 0x45d9f3b; 
+      x = (x >>> 16) ^ x;
+      return x;
+  }
+
+  let hash = mix(valeurBase);
+
+  let perturbation = (annee * 31 + mois * 59 + jour * 97) % plafond;
+  hash = (hash + perturbation) % plafond;
+
+  let nonLinearAdjustment = Math.floor((Math.sin(annee) * Math.cos(mois) * Math.tan(jour) * 1000) % plafond);
+  hash = (hash + nonLinearAdjustment) % plafond;
+
+  if (hash < 0) {
+      hash += plafond;
+  }
+
+  let uniformHash = (hash + (hash >>> 3) ^ (hash >>> 5)) % plafond;
+  if (uniformHash < 0) {
+      uniformHash += plafond;
+  }
+
+  return Math.floor(uniformHash);
 };
 
 const getDailyElement = () => {
   const today = new Date();
   const todayFrance = new Date(today.getTime()).toISOString().split('T')[0];
   const storedDate = localStorage.getItem('date');
-  const storedElement = localStorage.getItem('dailyElement');
 
-  if (storedDate === todayFrance && storedElement) {
-    return storedElement;
-  } else {
-    const newElement = getRandomElement(data);
+  if (storedDate !== todayFrance) {
     localStorage.clear();
-    localStorage.setItem('dailyElement', newElement);
     localStorage.setItem('date', todayFrance);
-    return newElement;
   }
+  return hashDate(todayFrance, data.length);
 };
 
 export default getDailyElement;
